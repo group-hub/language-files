a20226a3f0f0f4dd4eafc6c7acff631efe6413a8
John Wheal <john@whealmedia.com>
2016-05-23 11:45:23 +0100

    Removed meeting language

--- en-gb/events.php
+++ en-gb/events.php
@@ -11,6 +11,16 @@ return [
 	|
 	*/
 
+	'name' => 'Name',
+	'location' => 'Location',
+	'start-time' => 'Start Time',
+	'end-time' => 'End Time',
+	'start-date' => 'Start Date',
+	'end-date' => 'End Date',
+	'agenda' => 'Agenda',
+	'minutes' => 'Minutes', //(Meeting minutes)
+	'notes' => 'Notes',
+
 	'description' => 'These are for one-off events',
 	'new' => 'New Event',
 	'edit' => 'Edit Event',
@@ -21,6 +31,11 @@ return [
 	'cost' => 'Cost',
 	'summary' => 'Summary',
 
+	'enter-location' => 'You must enter a location',
+	'enter-start-date' => 'Your must enter a valid start date',
+	'enter-start-time' => 'You must enter a valid start time',
+	'enter-end-date' => 'Your must enter a valid end date',
+	'enter-end-time' => 'You must enter a valid end time',
 	'enter-event-name' => 'You must enter an event name',
 	'enter-end-before-start' => 'The event must start before it ends!',
 
